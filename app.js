7f939cc204d4f54246dbe8f76e01a6f8843183d8
Iago Leobas <iago.leobass@gmail.com>
2023-04-10 14:37:30 -0300

    feat: new updates

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-const express = require("express");
+const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log(`${new Date().toISOString()}: Chegou request`);
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Formulário</title>
+      </head>
+      <body>
+        <form action="/" method="post">
+          <label for="texto">Digite um texto:</label>
+          <input type="text" id="texto" name="texto">
+          <button type="submit">Enviar</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/', (req, res) => {
+  const texto = req.body.texto;
+  fs.writeFileSync('logs/teste.txt', texto);
+  res.send(`
+    <html>
+      <head>
+        <title>Dados salvos</title>
+      </head>
+      <body>
+        <p>Dados salvos com sucesso.</p>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
